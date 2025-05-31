e5d13c8d5d4ddd2ff5057dea9bc930105f36a2c3
Daniella Fegi <114934334+Yuf0ria@users.noreply.github.com>
2025-05-31 21:51:25 +0800

    Added Three Doors an Extra Door
    DOOR COLORS:
    -Red -MAG
    -Black -INT
    -Cyan -CHA
    -White -Remove 2, 1 or all attributes

---
+++ Activity 1/Assets/AttributeManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttributeManager : MonoBehaviour
+{
+    public Text attributeDisplay;
+    static public int MAGIC = 16;
+    static public int INTELLIGENCE = 8;
+    static public int CHARISMA = 4;
+    int attributes = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(this.transform.position);
+        attributeDisplay.transform.position = screenPoint + new Vector3(0,-50,0);
+        attributeDisplay.text = Convert.ToString(attributes, 2).PadLeft(8, '0');
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Magic")
+        {
+            attributes |= MAGIC;
+        }
+        else if (other.gameObject.tag == "Intelligence")
+        {
+            attributes |= INTELLIGENCE;
+        }
+        else if (other.gameObject.tag == "Charisma")
+        {
+            attributes |= CHARISMA;
+        }
+        else if (other.gameObject.tag == "Invisible")
+        {
+            if (MAGIC > 0 && INTELLIGENCE > 0)
+            {
+                attributes |= MAGIC = 0;
+                attributes |= INTELLIGENCE = 0;
+            }
+            else
+            {
+                attributes = 0;
+            }
+        }
+    }
+
+}
